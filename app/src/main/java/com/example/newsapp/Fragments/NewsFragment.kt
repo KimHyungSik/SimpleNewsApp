3303d0c3b3b1381a00ee3ffa1e590e64d00dfed1
KimHyungSik <goust2001c@naver.com>
2021-04-14 16:53:15 +0900

    top app bar navigation icon null and backup before add room

--- app/src/main/java/com/example/newsapp/Fragments/NewsFragment.kt
+++ app/src/main/java/com/example/newsapp/Fragments/NewsFragment.kt
@@ -1,10 +1,13 @@
 package com.example.newsapp.Fragments
 
 import android.content.Intent
+import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.*
+import android.widget.EditText
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
@@ -14,9 +17,11 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.newsapp.R
 import com.example.newsapp.Recycler.InRecyclerView
 import com.example.newsapp.Recycler.NewsRecyclerAdapter
+import com.example.newsapp.Utils.RESPONSE_STATUIS
 import com.example.newsapp.Utils.Utility.TAG
 import com.example.newsapp.ViewModel.NewsFragmentViewModel
 import com.example.newsapp.databinding.FragmentNewsBinding
+import okhttp3.internal.wait
 
 class NewsFragment : Fragment(), InRecyclerView {
     private var mBinding: FragmentNewsBinding? = null
@@ -54,6 +59,10 @@ class NewsFragment : Fragment(), InRecyclerView {
 
         // onCreateOptionsMenu 활성화
         setHasOptionsMenu(true)
+        mBinding?.mainTopAppBar?.apply {
+            navigationIcon = null
+            title = "뉴스"
+        }
 
         return mBinding?.root
     }
@@ -78,7 +87,6 @@ class NewsFragment : Fragment(), InRecyclerView {
     // 메뉴 생성
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.main_top_bar, menu)
-        Log.d(TAG, "onCreateOptionsMenu: $menu")
         val searchItem = menu.findItem(R.id.search_top_bar_icon)
         val searchView = searchItem.actionView as SearchView
 
@@ -86,10 +94,10 @@ class NewsFragment : Fragment(), InRecyclerView {
             override fun onQueryTextSubmit(query: String?): Boolean {
                 Log.d(TAG, "onQueryTextSubmit: $query")
 
-                searchView.clearFocus()
-                mBinding?.mainTopAppBar?.title = query
-
                 mViewModel.filter(query!!)
+
+                searchView.clearFocus()
+                mBinding?.mainTopAppBar?.collapseActionView()
 
                 return true
             }
@@ -101,11 +109,19 @@ class NewsFragment : Fragment(), InRecyclerView {
 
         searchView.apply {
             this.queryHint = "검색어를 입력하세요"
+            this.findViewById<EditText>(androidx.appcompat.R.id.search_src_text).apply {
+                this.setTextColor(Color.WHITE)
+                this.setHintTextColor(Color.WHITE)
+            }
         }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        Log.d(TAG, "onOptionsItemSelected: ")
+        when(item.itemId){
+            android.R.id.home->{
+                Log.d(TAG, "onOptionsItemSelected: home click")
+            }
+        }
         return super.onOptionsItemSelected(item)
     }
 
--- app/src/main/java/com/example/newsapp/ViewModel/NewsFragmentViewModel.kt
+++ app/src/main/java/com/example/newsapp/ViewModel/NewsFragmentViewModel.kt
@@ -1,10 +1,12 @@
 package com.example.newsapp.ViewModel
 
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.newsapp.Model.NewsModel
 import com.example.newsapp.Retrofit.NewsRetrofitManager
+import com.example.newsapp.Utils.RESPONSE_STATUIS
 import com.example.newsapp.Utils.Utility.TAG
 
 class NewsFragmentViewModel: ViewModel() {
@@ -18,7 +20,17 @@ class NewsFragmentViewModel: ViewModel() {
 
     fun filter(query: String){
         NewsRetrofitManager.instance.searchHeadlinesNews(query, "kr", completion = { responsState, responseDataArrayList ->
-            _newsLiveData.postValue(responseDataArrayList)
+            when(responsState){
+                RESPONSE_STATUIS.OK ->{
+                    _newsLiveData.postValue(responseDataArrayList)
+                }
+                RESPONSE_STATUIS.NO_CONTENT ->{
+                    Log.d(TAG, "filter: 검색 데이터가 없습니다")
+                }
+                RESPONSE_STATUIS.ERROR ->{
+                    Log.d(TAG, "filter: 서버에러")
+                }
+            }
         })
     }
 
